1e2e863f30124d3f156a030e3427c0a7d81e519f
akshitap31 <parasrampuriaakshita95@gmail.com>
2020-09-20 18:39:44 -0400

    Adding

--- assets/js/dendrogram.js
+++ assets/js/dendrogram.js
@@ -1,56 +1,52 @@
-// set the dimensions and margins of the graph
-var width = 460
-var height = 460
-var radius = width / 2 // radius of the dendrogram
 
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + radius + "," + radius + ")");
 
-// read json data
-d3.json("../data/skill.json", function(data) {
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height"),
+    g = svg.append("g").attr("transform", "translate(" + (width / 2 - 15) + "," + (height / 2 + 25) + ")");
+
+var stratify = d3.stratify()
+    .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+
+var tree = d3.cluster()
+    .size([360, 390])
+    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+
+d3.csv("https://raw.githubusercontent.com/akshitap31/Portfolio-website/master/assets/data/skills.csv", function(error, data) {
+  if (error) throw error;
 console.log(data)
-  // Create the cluster layout:
-  var cluster = d3.cluster()
-    .size([360, radius - 60]);  // 360 means whole circle. radius - 60 means 60 px of margin around dendrogram
+  var root = tree(stratify(data)
+      .sort(function(a, b) { return (a.height - b.height) || a.id.localeCompare(b.id); }));
 
-  // Give the data to this cluster layout:
-  var root = d3.hierarchy(data, function(d) {
-      return d.children;
-  });
-  cluster(root);
+  var link = g.selectAll(".link")
+    .data(root.descendants().slice(1))
+    .enter().append("path")
+      .attr("class", "link")
+      .attr("d", function(d) {
+        return "M" + project(d.x, d.y)
+            + "C" + project(d.x, (d.y + d.parent.y) / 2)
+            + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
+            + " " + project(d.parent.x, d.parent.y);
+      });
 
-  // Features of the links between nodes:
-  var linksGenerator = d3.linkRadial()
-      .angle(function(d) { return d.x / 180 * Math.PI; })
-      .radius(function(d) { return d.y; });
+  var node = g.selectAll(".node")
+    .data(root.descendants())
+    .enter().append("g")
+      .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+      .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; });
 
-  // Add the links between nodes:
-  svg.selectAll('path')
-    .data(root.links())
-    .enter()
-    .append('path')
-      .attr("d", linksGenerator)
-      .style("fill", 'none')
-      .attr("stroke", '#ccc')
+  node.append("circle")
+      .attr("r", 2.5);
 
+  node.append("text")
+      .attr("dy", ".31em")
+      .attr("x", function(d) { return d.x < 180 === !d.children ? 6 : -6; })
+      .style("text-anchor", function(d) { return d.x < 180 === !d.children ? "start" : "end"; })
+      .attr("transform", function(d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; })
+      .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
+});
 
-  // Add a circle for each node.
-  svg.selectAll("g")
-      .data(root.descendants())
-      .enter()
-      .append("g")
-      .attr("transform", function(d) {
-          return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
-      })
-      .append("circle")
-        .attr("r", 7)
-        .style("fill", "#69b3a2")
-        .attr("stroke", "black")
-        .style("stroke-width", 2)
-
-})
+function project(x, y) {
+  var angle = (x - 90) / 180 * Math.PI, radius = y;
+  return [radius * Math.cos(angle), radius * Math.sin(angle)];
+}
